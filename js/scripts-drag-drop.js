e45aa98883589cef379a50229dd6482c58807a1d
shipraguinaskui <shipra.guin@askui.com>
2024-05-13 17:05:59 +0200

    changes updated

--- js/scripts-drag-drop.js
+++ js/scripts-drag-drop.js
@@ -1,17 +1,17 @@
 function allowDrop(elem) {
-    elem.preventDefault();
-  }
-  
-  function drag(elem) {
-    elem.dataTransfer.setData("text", elem.target.id);
-  }
-  
-  function drop(elem) {
-    elem.preventDefault();
-    var data = elem.dataTransfer.getData("text");
-    elem.target.appendChild(document.getElementById(data));
-    const dropHereText = Array.from(elem.target.childNodes).find(node => node.nodeType === Node.TEXT_NODE);
+  elem.preventDefault();
+}
+
+function drag(elem) {
+  elem.dataTransfer.setData("text", elem.target.id);
+}
+
+function drop(elem) {
+  elem.preventDefault();
+  let data = elem.dataTransfer.getData("text");
+  elem.target.appendChild(document.getElementById(data));
+  const dropHereText = Array.from(elem.target.childNodes).find(node => node.nodeType === Node.TEXT_NODE);
   if (dropHereText && dropHereText.textContent.trim() === "Drop here") {
     dropHereText.parentNode.removeChild(dropHereText);
   }
-  }
+}
